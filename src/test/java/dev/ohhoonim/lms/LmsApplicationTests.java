f9b80dc5e9f7c9972c819f083006ec4599022498
mathew <donnj95@gmail.com>
2024-10-10 17:10:13 +0900

    querydsl 추가

--- src/test/java/dev/ohhoonim/lms/LmsApplicationTests.java
+++ src/test/java/dev/ohhoonim/lms/LmsApplicationTests.java
@@ -2,12 +2,14 @@ package dev.ohhoonim.lms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootTest
 class LmsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ApplicationModules.of(LmsApplication.class).verify();
 	}
 
 }
