66ad52e2c5e877f982048de316475f14f6163c19
mathew <donnj95@gmail.com>
2024-11-18 17:25:08 +0900

    설계관련 부분 태그로 저장 Fixes #37 설계문서들은 lms-design 리파지토리로 이동

--- src/main/java/dev/ohhoonim/lms/domain/learningCourses/model/exception/NotFound.java
+++
@@ -1,9 +0,0 @@
-package dev.ohhoonim.lms.domain.learningCourses.model.exception;
-
-public class NotFound extends RuntimeException{
-
-    public NotFound(String message) {
-        super(message);
-    }
-
-}
