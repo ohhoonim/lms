aef02dfdf9cacdc04a9715326cd3aae79650d8ae
mathew <donnj95@gmail.com>
2025-07-05 13:21:26 +0900

    코드리뷰 2

--- src/main/java/dev/ohhoonim/component/id/Id.java
+++ src/main/java/dev/ohhoonim/component/id/Id.java
@@ -6,5 +6,12 @@ public class Id {
 
     private UUID id;
 
+    public Id() {
+        this.id = UUID.randomUUID();
+    }
 
+    @Override
+    public String toString() {
+        return id.toString();
+    }
 }
